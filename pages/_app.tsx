46d33c17e6777fb004abfb3364d99dbcc95e1e60
Luis Assis <105894842+DovahKiinCat@users.noreply.github.com>
2023-01-28 23:22:19 -0300

    Mobile Test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <div>
+  <div className='overflow-hidden'>
       <Header/>
       <Component {...pageProps} />
   </div>)
