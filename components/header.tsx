845689aa19ea814593cc959c5287f51b4e3508d3
Luis Assis <105894842+DovahKiinCat@users.noreply.github.com>
2023-01-28 22:13:03 -0300

    Mobile Test

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion"
 
 export default function Header() {
     return (
-        <header className='w-full absolute'>
+        <header className='w-screen absolute md:w-full'>
             <motion.div 
                 initial={{ opacity: 0, y: -80 }}
                 animate={{ opacity: 1, y: 0 }}
