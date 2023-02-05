91c9cdec9d707437b9da063770bfedd96633d14e
Luis Assis <105894842+DovahKiinCat@users.noreply.github.com>
2023-02-05 02:05:05 -0300

    form test

--- pages/email/contactus.tsx
+++ pages/email/contactus.tsx
@@ -2,6 +2,7 @@ import Layout from "layout/layout"
 import { useForm, ValidationError } from "@formspree/react"
 import Typewriter from "typewriter-effect"
 import Link from "next/link"
+import { motion } from "framer-motion"
 
 export default function ContactForm() {
 
@@ -34,96 +35,97 @@ export default function ContactForm() {
         </Layout>
 
         <section className="flex-1 flex flex-col justify-center h-screen w-full">
-
             <section className="mx-auto">
-                <section className="relative bg-white w-[350px] h-[500px] rounded-2xl shadow-xl top-[-25px] lg:w-[700px] lg:h-[500px] lg:top-[-45px]"> 
-                    <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-16 p-4 lg:mt-20 min-w-full lg:min-w-[500px]">
-                        <input 
-                            id="companyName"
-                            name="companyName"
-                            required
-                            maxLength={128}
-                            type="text"
-                            placeholder="Company name"
-                            className="outline-none border-2 rounded-2xl px-8 py-2
-                            
-                            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                            invalid:border-pink-500 invalid:text-pink-600
-                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+                <motion.div
+                    initial={{opacity: 0, y: -30}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{delay: 0.25}}
+                >
+                    <section className="relative bg-white w-[350px] h-[500px] rounded-2xl shadow-xl top-[-25px] lg:w-[700px] lg:h-[500px] lg:top-[-45px]"> 
+                        <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-16 p-4 lg:mt-20 min-w-full lg:min-w-[500px]">
+                            <input 
+                                id="companyName"
+                                name="companyName"
+                                required
+                                maxLength={128}
+                                type="text"
+                                placeholder="Company name"
+                                className="outline-none border-2 rounded-2xl px-8 py-2
+                                
+                                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                invalid:border-pink-500 invalid:text-pink-600
+                                focus:invalid:border-pink-500 focus:invalid:ring-pink-500
 
-                            "
-                        />
+                                "
+                            />
 
-                        <input 
-                            id="email"
-                            name="email"
-                            type="email"
-                            required
-                            maxLength={128}
-                            placeholder="Your E-mail"
-                            className="outline-none border-2 rounded-2xl px-8 py-2
-                            
-                            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                            invalid:border-pink-500 invalid:text-pink-600
-                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-                            peer
-                            
-                            "
-                        />
+                            <input 
+                                id="email"
+                                name="email"
+                                type="email"
+                                required
+                                maxLength={128}
+                                placeholder="Your E-mail"
+                                className="outline-none border-2 rounded-2xl px-8 py-2
+                                
+                                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                invalid:border-pink-500 invalid:text-pink-600
+                                focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+                                peer
+                                
+                                "
+                            />
 
-                        <p className="mx-auto invisible peer-invalid:visible text-pink-600 text-sm">
-                            Please provide a valid email address.
-                        </p>
+                            <p className="mx-auto invisible peer-invalid:visible text-pink-600 text-sm">
+                                Please provide a valid email address.
+                            </p>
 
-                        <textarea 
-                            id="message" 
-                            name="message"
-                            required
-                            maxLength={1048576}
-                            placeholder="Additional information"
-                            className="border-2 outline-none rounded-2xl px-8 py-6 min-h-[16em] scrollbar hover:scrollbar-thumb-gray-900 scrollbar-thumb-black scrollbar-track-gray-200
-                            
-                            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                            invalid:border-pink-500 invalid:text-pink-600
-                            focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-                            
-                            ">
-                        </textarea>
+                            <textarea 
+                                id="message" 
+                                name="message"
+                                required
+                                maxLength={1048576}
+                                placeholder="Additional information"
+                                className="border-2 outline-none rounded-2xl px-8 py-6 min-h-[16em] scrollbar hover:scrollbar-thumb-gray-900 scrollbar-thumb-black scrollbar-track-gray-200
+                                
+                                focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                                invalid:border-pink-500 invalid:text-pink-600
+                                focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+                                
+                                ">
+                            </textarea>
 
-                        <ValidationError
-                            prefix="Email"
-                            field="email"
-                            errors={state.errors}
-                        />
+                            <ValidationError
+                                prefix="Email"
+                                field="email"
+                                errors={state.errors}
+                            />
 
-                        <ValidationError 
-                            prefix="Message"
-                            field="message"
-                            errors={state.errors}
-                        />
+                            <ValidationError 
+                                prefix="Message"
+                                field="message"
+                                errors={state.errors}
+                            />
 
-                    <section className="flex justify-between text-center p-0 lg:p-0">
-                        <Link href={"../"}>
-                            <button className="relative text-black border-2 border-sky-500 font-bold rounded-2 shadow-xl rounded-2xl w-28 h-12">
-                                Home
-                            </button>
-                        </Link>
-                        
+                            <section className="flex justify-between text-center p-0 lg:p-0">
+                                <Link href={"../"}>
+                                    <button className="relative text-black border-2 border-sky-500 font-bold rounded-2 shadow-xl rounded-2xl w-28 h-12">
+                                        Home
+                                    </button>
+                                </Link>
+                                
 
-                        <button type="submit" disabled={state.submitting} className="relative text-black border-2 border-sky-500 font-bold rounded-2 shadow-xl rounded-2xl w-28 h-12">
-                            Enviar
-                        </button>
-
+                                <button type="submit" disabled={state.submitting} className="relative text-black border-2 border-sky-500 font-bold rounded-2 shadow-xl rounded-2xl w-28 h-12">
+                                    Enviar
+                                </button>
+                            </section>
+                        </form>
                     </section>
-
-                    </form>
-                </section>
+                </motion.div>    
             </section>
-
-
         </section>
         </>
     )
